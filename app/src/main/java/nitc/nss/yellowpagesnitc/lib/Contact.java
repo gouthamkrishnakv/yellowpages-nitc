f978d0a99a5a1e46d9624de41d2ba997a5d9a619
Farzeen <happycoder97@protonmail.com>
2018-10-15 23:53:07 +0530

    Simplifying hierarchy. Credits: Prabodh TR

---
+++ app/src/main/java/nitc/nss/yellowpagesnitc/lib/Contact.java
@@ -0,0 +1,9 @@
+package nitc.nss.yellowpagesnitc.lib;
+
+import java.util.ArrayList;
+
+public class Contact {
+    public String name;
+    public ArrayList<String> number;
+    public String notes;
+}
